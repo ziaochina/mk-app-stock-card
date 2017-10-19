eb752cd6fcc270cbfe1fc06fd378b979327f5075
ziaochina <ziaochina@gmail.com>
2017-10-19 17:52:46 +0800

    zlj

--- extend.js
+++ extend.js
@@ -5,70 +5,73 @@ function getGridOption() {
 		'unitGrid': {
 			path: 'data.form.units',
 			selectFieldName: 'selected',
-			colNames: [
-				'unit',
-				'conversionRate',
-				'conversionDesc',
-				'isBase',
-				'isPu',
-				'isSa',
-				'isSt'
-			],
-			colCount: 7,
 			cellClassName: 'mk-app-stock-card-cell',
-			emptyRow: {}
+			emptyRow: {},
+			getColNames: (gf) => {
+				return [
+					'unit',
+					'conversionRate',
+					'conversionDesc',
+					'isBase',
+					'isPu',
+					'isSa',
+					'isSt'
+				]
+			},
 		},
 		'priceGrid': {
 			path: 'data.form.prices',
 			selectFieldName: 'selected',
-			colNames: [
-				'unit',
-				'lastPuPrice',
-				'referPuPrice',
-				'lastSaPrice',
-				'retailPrice',
-				'firstTradePrice',
-				'secondTradePrice',
-				'thirdTradePrice',
-				'fourthTradePrice',
-				'fifthTradePrice',
-				'sixthTradePrice',
-				'servenTradePrice',
-				'eighthTradePrice',
-				'ninthTradePrice',
-				'tenthTradePrice',
-			],
-			colCount: 15,
 			cellClassName: 'mk-app-stock-card-cell',
-			emptyRow: {}
+			emptyRow: {},
+			getColNames: (gf) => {
+				return [
+					'unit',
+					'lastPuPrice',
+					'referPuPrice',
+					'lastSaPrice',
+					'retailPrice',
+					'firstTradePrice',
+					'secondTradePrice',
+					'thirdTradePrice',
+					'fourthTradePrice',
+					'fifthTradePrice',
+					'sixthTradePrice',
+					'servenTradePrice',
+					'eighthTradePrice',
+					'ninthTradePrice',
+					'tenthTradePrice',
+				]
+			},
 		},
 		'barcodeGrid': {
 			path: 'data.form.barcodes',
 			selectFieldName: 'selected',
-			colNames: [
-				'barcode',
-				'unit'
-			],
-			colCount:2,
 			cellClassName: 'mk-app-stock-card-cell',
-			emptyRow: {}
+			emptyRow: {},
+			getColNames: (gf) => {
+				return [
+					'barcode',
+					'unit'
+				]
+			},
 		}
 	}
 }
 
-function actionCreator(option){
-    return {
-        gridAction: new aarGrid.action({...option, gridOption: getGridOption()})
-    }
+function actionCreator(option) {
+	return {
+		gridAction: new aarGrid.action({ ...option, gridOption: getGridOption() })
+	}
 }
 
-function reducerCreator(option){
-    return {
-        gridReducer: new aarGrid.reducer({...option, gridOption: getGridOption()})
-    }
+function reducerCreator(option) {
+	return {
+		gridReducer: new aarGrid.reducer({ ...option, gridOption: getGridOption() })
+	}
 }
 
 export default {
-    actionCreator,
-    reducerCreator
+	actionCreator,
+	reducerCreator
 }
